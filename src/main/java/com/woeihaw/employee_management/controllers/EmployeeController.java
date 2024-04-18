e5b8b64e799e6103f224f5df08409dfcd0ae7445
Ang <woeihaw94@gmail.com>
2024-04-18 21:34:35 +0800

    modification in gradle

--- src/main/java/com/woeihaw/employee_management/controllers/EmployeeController.java
+++ src/main/java/com/woeihaw/employee_management/controllers/EmployeeController.java
@@ -24,6 +24,7 @@ public class EmployeeController {
     private EmployeeService empService ;
     @GetMapping({"","/"})
     public String showEmployeeList(Model model) throws InterruptedException {
+
         List<Employee> employees= empService.getAllEmployee();
         model.addAttribute("employees",employees);
 
